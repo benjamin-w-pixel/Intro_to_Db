ba403e0a489269637de028fc0cc3c6ef94b2c86e
Benjamin W. Pixel <benjamin.w.pixel@example.com>
2025-07-24 13:24:15 +0300

    this is introduction to data base

---
+++ task_5.sql
@@ -0,0 +1,5 @@
+-- File: task_5.sql
+-- --- a/file:///c%3A/Users/hp/Intro_to_Db/task_5.sql
+
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
